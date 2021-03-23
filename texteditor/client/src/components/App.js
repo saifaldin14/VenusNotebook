4b25332b016ae5108971d9ef515c6b43f6a76ebe
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-23 09:10:25 -0400

    Update App.js

--- texteditor/client/src/components/App.js
+++ texteditor/client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
 // pages for this product
@@ -6,21 +6,32 @@ import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
-import Footer from "./views/Footer/Footer"
+import Footer from "./views/Footer/Footer";
 
-//null   Anyone Can go inside
-//true   only logged in user can go inside
-//false  logged in user can't go inside
+import PostPage from "./views/PostPage/PostPage";
+import BlogPage from "./views/BlogPage/BlogPage";
+import CreateBlogPage from "./views/BlogPage/Section.js/CreatePage";
 
 function App() {
   return (
-    <Suspense fallback={(<div>Loading...</div>)}>
+    <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/blog" component={Auth(BlogPage, null)} />
+          <Route
+            exact
+            path="/blog/create"
+            component={Auth(CreateBlogPage, null)}
+          />
+          <Route
+            exact
+            path="/blog/post/:postId"
+            component={Auth(PostPage, null)}
+          />
         </Switch>
       </div>
       <Footer />
