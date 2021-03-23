08b4e64749a03579389ae5a2dfe7492677335a33
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-23 09:13:07 -0400

    Update LeftMenu.js

--- texteditor/client/src/components/views/NavBar/Sections/LeftMenu.js
+++ texteditor/client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import { Menu } from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+import React from "react";
+import { Menu } from "antd";
 
 function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
-    <Menu.Item key="mail">
-      <a href="/">Home</a>
-    </Menu.Item>
-    <SubMenu title={<span>Blogs</span>}>
-      <MenuItemGroup title="Item 1">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
-      </MenuItemGroup>
-      <MenuItemGroup title="Item 2">
-        <Menu.Item key="setting:3">Option 3</Menu.Item>
-        <Menu.Item key="setting:4">Option 4</Menu.Item>
-      </MenuItemGroup>
-    </SubMenu>
-  </Menu>
-  )
+      <Menu.Item key="mail">
+        <a href="/">Home</a>
+      </Menu.Item>
+      <Menu.Item key="blog">
+        <a href="/blog">Blog</a>
+      </Menu.Item>
+      <Menu.Item key="create">
+        <a href="/blog/create">Create</a>
+      </Menu.Item>
+    </Menu>
+  );
 }
 
-export default LeftMenu
+export default LeftMenu;
