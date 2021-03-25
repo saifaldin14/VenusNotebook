3b45be8ae7ed06ada154ab8e9f9d3e07b43f6af3
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-24 21:29:46 -0400

    Update Footer.js

--- texteditor/client/src/components/views/Footer/Footer.js
+++ texteditor/client/src/components/views/Footer/Footer.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import {Icon} from 'antd';
+import React from "react";
+import { Icon } from "antd";
 
 function Footer() {
-    return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize:'1rem'
-        }}>
-           <p> Happy Coding  <Icon type="smile" /></p>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        height: "80px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "1rem",
+      }}
+    >
+      <p>
+        {" "}
+        Let's make something great <Icon type="smile" />
+      </p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
