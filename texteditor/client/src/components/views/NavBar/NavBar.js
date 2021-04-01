687c609c934aef6eeda2c5c4eec7954f2798bcc0
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-31 20:02:10 -0400

    Update NavBar.js

--- texteditor/client/src/components/views/NavBar/NavBar.js
+++ texteditor/client/src/components/views/NavBar/NavBar.js
@@ -16,10 +16,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav
-      className="menu"
-      style={{ position: "fixed", zIndex: 5, width: "100%" }}
-    >
+    <nav className="menu" style={{ zIndex: 1, width: "100%" }}>
       <div className="menu__logo">
         <a href="/">Logo</a>
       </div>
