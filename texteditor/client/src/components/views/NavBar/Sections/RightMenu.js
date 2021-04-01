7a6e25003645596c55b38c72839aa00f258f9a35
saifaldin14 <saifaldindeveloper@gmail.com>
2021-03-31 20:00:47 -0400

    Update RightMenu.js

--- texteditor/client/src/components/views/NavBar/Sections/RightMenu.js
+++ texteditor/client/src/components/views/NavBar/Sections/RightMenu.js
@@ -1,20 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import { Menu } from 'antd';
-import axios from 'axios';
-import { USER_SERVER } from '../../../Config';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { Menu } from "antd";
+import axios from "axios";
+import { USER_SERVER } from "../../../Config";
+import { withRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-function RightMenu(props) {
-  const user = useSelector(state => state.user)
+const RightMenu = (props) => {
+  const user = useSelector((state) => state.user);
 
   const logoutHandler = () => {
-    axios.get(`${USER_SERVER}/logout`).then(response => {
+    axios.get(`${USER_SERVER}/logout`).then((response) => {
       if (response.status === 200) {
         props.history.push("/login");
       } else {
-        alert('Log Out Failed')
+        alert("Log Out Failed");
       }
     });
   };
@@ -23,13 +23,13 @@ function RightMenu(props) {
     return (
       <Menu mode={props.mode}>
         <Menu.Item key="mail">
-          <a href="/login">Signin</a>
+          <a href="/login">Sign In</a>
         </Menu.Item>
         <Menu.Item key="app">
-          <a href="/register">Signup</a>
+          <a href="/register">Sign Up</a>
         </Menu.Item>
       </Menu>
-    )
+    );
   } else {
     return (
       <Menu mode={props.mode}>
@@ -37,9 +37,8 @@ function RightMenu(props) {
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
       </Menu>
-    )
+    );
   }
-}
+};
 
 export default withRouter(RightMenu);
-
