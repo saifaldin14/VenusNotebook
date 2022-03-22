5c212be7e1d3c54536555018a53e0e2bc98bc218
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-22 09:42:27 -0400

    Create index.js

---
+++ packages/local-api/dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serve = void 0;
+var express_1 = __importDefault(require("express"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
+var path_1 = __importDefault(require("path"));
+var cells_1 = require("./routes/cells");
+var serve = function (port, filename, dir, useProxy) {
+  var app = (0, express_1.default)();
+  app.use((0, cells_1.createCellsRouter)(filename, dir));
+  if (useProxy) {
+    app.use(
+      (0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: "http://localhost:3000",
+        ws: true,
+        logLevel: "silent",
+      })
+    );
+  } else {
+    var packagePath = require.resolve("@jsnote/local-client/build/index.html");
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+  }
+  return new Promise(function (resolve, reject) {
+    app.listen(port, resolve).on("error", reject);
+  });
+};
+exports.serve = serve;
