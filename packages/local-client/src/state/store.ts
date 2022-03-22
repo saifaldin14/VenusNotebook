0c4756cb6c2d00b4d645d5ac4652a29a60645b6d
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-22 10:05:59 -0400

    Create store.ts

---
+++ packages/local-client/src/state/store.ts
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+import { persistMiddlware } from "./middlewares/persist-middlware";
+
+export const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(persistMiddlware, thunk)
+);
