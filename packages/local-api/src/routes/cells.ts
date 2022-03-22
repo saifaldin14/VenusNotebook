f30a50f313bca632614e9314dde173aaa9da5ed1
saifaldin14 <saifaldindeveloper@gmail.com>
2022-03-22 09:54:45 -0400

    Create cells.ts

---
+++ packages/local-api/src/routes/cells.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+
+interface Cell {
+  id: string;
+  content: string;
+  type: "text" | "code";
+}
+
+export const createCellsRouter = (filename: string, dir: string) => {
+  const router = express.Router();
+  router.use(express.json());
+
+  const fullPath = path.join(dir, filename);
+
+  router.get("/cells", async (req, res) => {
+    try {
+      // Read the file
+      const result = await fs.readFile(fullPath, { encoding: "utf-8" });
+
+      res.send(JSON.parse(result));
+    } catch (err: any) {
+      if (err.code === "ENOENT") {
+        await fs.writeFile(fullPath, "[]", "utf-8");
+        res.send([]);
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  router.post("/cells", async (req, res) => {
+    // Take the list of cells from the request obj
+    // serialize them
+    const { cells }: { cells: Cell[] } = req.body;
+
+    // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+
+    res.send({ status: "ok" });
+  });
+
+  return router;
+};
